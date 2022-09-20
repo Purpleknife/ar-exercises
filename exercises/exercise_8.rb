212ec376ef1374a04ed522211f148d59f1a56afe
Hind <hind.nayhi@gmail.com>
2022-09-19 18:19:07 -0600

    Give the employees a password before their record is created.

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -9,3 +9,15 @@ require_relative './exercise_7'
 
 # To practice AR Callbacks:
 
+# We want to be able to give employees a password (string)
+# The password is auto-generated when their record is created in the database.
+
+
+puts "Exercise 8"
+puts "----------"
+
+@new_employee = @store2.employees.create(first_name: "Buffy", last_name: "Summers", hourly_rate: 40)
+
+puts "The new employee's password is:"
+print "> "
+puts @new_employee.password
